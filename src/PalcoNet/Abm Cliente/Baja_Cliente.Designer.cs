50869542d4ff34a382bf5fe32e13e1f886313298
Mariano <mariano.ingrassia.91@gmail.com>
2018-12-04 02:15:43 -0200

    .

---
+++ src/PalcoNet/Abm Cliente/Baja_Cliente.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace PalcoNet.Abm_Cliente
+{
+    partial class Baja_Cliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.busquedaNombre_textBox = new System.Windows.Forms.TextBox();
+            this.busquedaApellido_textBox = new System.Windows.Forms.TextBox();
+            this.busquedaDNI_textBox = new System.Windows.Forms.TextBox();
+            this.busquedaEmail_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.busquedaCliente_button = new System.Windows.Forms.Button();
+            this.darDeBaja_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // busquedaNombre_textBox
+            // 
+            this.busquedaNombre_textBox.Location = new System.Drawing.Point(32, 79);
+            this.busquedaNombre_textBox.Name = "busquedaNombre_textBox";
+            this.busquedaNombre_textBox.Size = new System.Drawing.Size(100, 20);
+            this.busquedaNombre_textBox.TabIndex = 0;
+            this.busquedaNombre_textBox.TextChanged += new System.EventHandler(this.busquedaNombre_textBox_TextChanged);
+            // 
+            // busquedaApellido_textBox
+            // 
+            this.busquedaApellido_textBox.Location = new System.Drawing.Point(138, 79);
+            this.busquedaApellido_textBox.Name = "busquedaApellido_textBox";
+            this.busquedaApellido_textBox.Size = new System.Drawing.Size(100, 20);
+            this.busquedaApellido_textBox.TabIndex = 1;
+            this.busquedaApellido_textBox.TextChanged += new System.EventHandler(this.busquedaApellido_textBox_TextChanged);
+            // 
+            // busquedaDNI_textBox
+            // 
+            this.busquedaDNI_textBox.Location = new System.Drawing.Point(244, 79);
+            this.busquedaDNI_textBox.Name = "busquedaDNI_textBox";
+            this.busquedaDNI_textBox.Size = new System.Drawing.Size(100, 20);
+            this.busquedaDNI_textBox.TabIndex = 2;
+            this.busquedaDNI_textBox.TextChanged += new System.EventHandler(this.busquedaDNI_textBox_TextChanged);
+            // 
+            // busquedaEmail_textBox
+            // 
+            this.busquedaEmail_textBox.Location = new System.Drawing.Point(350, 79);
+            this.busquedaEmail_textBox.Name = "busquedaEmail_textBox";
+            this.busquedaEmail_textBox.Size = new System.Drawing.Size(100, 20);
+            this.busquedaEmail_textBox.TabIndex = 3;
+            this.busquedaEmail_textBox.TextChanged += new System.EventHandler(this.busquedaEmail_textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DNI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(347, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // busquedaCliente_button
+            // 
+            this.busquedaCliente_button.Location = new System.Drawing.Point(481, 121);
+            this.busquedaCliente_button.Name = "busquedaCliente_button";
+            this.busquedaCliente_button.Size = new System.Drawing.Size(75, 23);
+            this.busquedaCliente_button.TabIndex = 8;
+            this.busquedaCliente_button.Text = "Buscar";
+            this.busquedaCliente_button.UseVisualStyleBackColor = true;
+            this.busquedaCliente_button.Click += new System.EventHandler(this.busquedaCliente_button_Click);
+            // 
+            // darDeBaja_button
+            // 
+            this.darDeBaja_button.Location = new System.Drawing.Point(481, 384);
+            this.darDeBaja_button.Name = "darDeBaja_button";
+            this.darDeBaja_button.Size = new System.Drawing.Size(75, 23);
+            this.darDeBaja_button.TabIndex = 10;
+            this.darDeBaja_button.Text = "Dar de baja";
+            this.darDeBaja_button.UseVisualStyleBackColor = true;
+            this.darDeBaja_button.Click += new System.EventHandler(this.darDeBaja_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 174);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(524, 195);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Baja_Cliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 419);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.darDeBaja_button);
+            this.Controls.Add(this.busquedaCliente_button);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.busquedaEmail_textBox);
+            this.Controls.Add(this.busquedaDNI_textBox);
+            this.Controls.Add(this.busquedaApellido_textBox);
+            this.Controls.Add(this.busquedaNombre_textBox);
+            this.Name = "Baja_Cliente";
+            this.Text = "Baja_Cliente";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox busquedaNombre_textBox;
+        private System.Windows.Forms.TextBox busquedaApellido_textBox;
+        private System.Windows.Forms.TextBox busquedaDNI_textBox;
+        private System.Windows.Forms.TextBox busquedaEmail_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button busquedaCliente_button;
+        private System.Windows.Forms.Button darDeBaja_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ src/PalcoNet/Abm Cliente/Baja_Cliente.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PalcoNet.Abm_Cliente
+{
+    public partial class Baja_Cliente : Form
+    {
+        public Baja_Cliente()
+        {
+            InitializeComponent();
+        }
+
+        List<string> condiciones = new List<string>();
+        private string nombre_busqueda = "";
+        private string apellido_busqueda = "";
+        private string dni_busqueda = "";
+        private string email_busqueda = "";
+        string id;
+        int idAEliminar;
+
+        private void busquedaNombre_textBox_TextChanged(object sender, EventArgs e)
+        {
+            this.nombre_busqueda = busquedaNombre_textBox.Text;
+        }
+
+        private void busquedaApellido_textBox_TextChanged(object sender, EventArgs e)
+        {
+            this.apellido_busqueda = busquedaApellido_textBox.Text;
+        }
+
+        private void busquedaDNI_textBox_TextChanged(object sender, EventArgs e)
+        {
+            this.dni_busqueda = busquedaDNI_textBox.Text;
+        }
+
+        private void busquedaEmail_textBox_TextChanged(object sender, EventArgs e)
+        {
+            this.email_busqueda = busquedaEmail_textBox.Text;
+        }
+
+        private void busquedaCliente_button_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter data;
+
+            condiciones.Add("cliente_nombre like \'%" + this.nombre_busqueda + "%\'");
+            condiciones.Add("cliente_apellido like \'%" + this.apellido_busqueda + "%\'");
+            if (this.dni_busqueda != string.Empty)
+            {
+                condiciones.Add("cliente_numero_dni = " + this.dni_busqueda + "");
+            }
+            condiciones.Add("cliente_email like \'%" + this.email_busqueda + "%\'");
+
+            data = App.db.getCliente(condiciones);
+            DataTable tabla = new DataTable();
+            data.Fill(tabla);
+                        
+           // this.dataGridView1.Columns[0].Visible = false;
+            this.dataGridView1.DataSource = tabla;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.Columns[0].Visible = false;
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            condiciones.Clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = dataGridView1.Rows[e.RowIndex].Cells[0].FormattedValue.ToString();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           // pruebaValor = dataGridView1.Rows[e.RowIndex].Cells[0].FormattedValue.ToString();
+        }
+
+        private void darDeBaja_button_Click(object sender, EventArgs e)
+        {
+            idAEliminar = Convert.ToInt32(id);
+            MessageBox.Show("El valor seleccionado es : " + idAEliminar);
+            App.db.bajaCliente(idAEliminar);
+        }
+    }
+}
