10dab738134bcbd94bbc3ed10db585a2ff48e815
nicolasreyd <nicolasreydominguez@gmail.com>
2018-12-09 22:39:33 -0300

    Fix funcionalidades login

--- src/PalcoNet/Login/MenuPrincipal.cs
+++ src/PalcoNet/Login/MenuPrincipal.cs
@@ -15,6 +15,7 @@ namespace PalcoNet.Login
         public MenuPrincipal()
         {
             InitializeComponent();
+            validar_funcionalidades_disp();
         }
 
         public void validar_funcionalidades_disp()
@@ -112,7 +113,7 @@ namespace PalcoNet.Login
 
         private void MenuPrincipal_Load(object sender, EventArgs e)
         {
-            validar_funcionalidades_disp();
+            
         }
 
         private void ABMRol_Button_Click(object sender, EventArgs e)
